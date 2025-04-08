5f648b37e2e1e7e5f8617214fbdc1741dd65e28b
raheebgill29 <140135207+raheebgill29@users.noreply.github.com>
2025-04-08 12:18:01 +0500

    Rag page

--- app/rag-application/page.tsx
+++ app/rag-application/page.tsx
@@ -5,18 +5,21 @@ import Features from "./sections/features";
 import Ideas from "./sections/ideas";
 import Metrics from "./sections/metrics";
 // import Security from "./sections/security";
-import WhySection from "../sections/WhySection";
+// import WhySection from "../sections/WhySection";
 // import RagStatsSection from "./sections/rag-stats";
 import QuantumSection from "./sections/quantum";
 import FAQ from "./sections/faq";
 import SmallFoot from "../sections/smallFoot";
+import TechniquesSection from "./sections/TechniquesSection";
+import WhySection from "./sections/WhySection";
 export default function RagApplication() {
     return (
         <main className="w-full">
             <RagHero />
             <Features />
             <Ideas />
-            <Metrics />
+            <TechniquesSection/>
+            {/* <Metrics /> */}
             {/* <Security /> */}
             <WhySection />
             {/* <RagStatsSection /> */}
