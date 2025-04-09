0f5d74aa6a2d7e89efa439fab42d4e8e4d0300d4
raheebgill29 <140135207+raheebgill29@users.noreply.github.com>
2025-04-09 14:54:56 +0500

    View blogs

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       {/* <PricingSection /> */}
       {/* <PngSection /> */}
 
-      <BlogsSection />
+      {/* <BlogsSection /> */}
       {/* <TestimonialsSection /> */}
       <ContactSection />
 
