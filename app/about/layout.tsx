63e68b30b71370fc8914a6bbef52c784050b8fbe
Talha Shahbaz <admin@talhacodestudios.com>
2025-04-16 14:16:17 +0500

    About us updated

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "About Us - Synoptix.ai",
-  description: "Learn about Synoptix.ai and our mission",
+  title: "About Us Synoptix.AI | Enterprise AI Platform & Solution",
+  description: "Learn how Synoptix.AI empowers enterprises with secure, no‑code AI automation, instant chat, and customizable AI agents. Discover our mission and vision.",
   alternates: {
     canonical: '/about',
   },
