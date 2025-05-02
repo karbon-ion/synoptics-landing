4187378ac2a87ab57703f9eebe2ce028af670443
raheebgill29 <140135207+raheebgill29@users.noreply.github.com>
2025-05-02 10:26:25 +0500

    fix:routing

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,24 @@ export default function RootLayout({
         <noscript>
           <img height="1" width="1" style={{display: "none"}} alt="" src="https://px.ads.linkedin.com/collect/?pid=7172076&fmt=gif" />
         </noscript>
+        
+        {/* Microsoft Bing UET (Universal Event Tracking) */}
+        <Script id="microsoft-bing-uet" strategy="afterInteractive">
+          {`
+            (function(w,d,t,r,u) {
+              var f,n,i;
+              w[u]=w[u]||[],f=function() {
+                var o={ti:"97184883", enableAutoSpaTracking: true};
+                o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")
+              },
+              n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function() {
+                var s=this.readyState;
+                s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)
+              },
+              i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)
+            })(window,document,"script","//bat.bing.com/bat.js","uetq");
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
         <FontAwesome />
