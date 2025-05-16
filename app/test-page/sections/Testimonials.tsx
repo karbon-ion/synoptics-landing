5a95e82d6777aeb3467d86658fed1f425755c832
raheebgill29 <140135207+raheebgill29@users.noreply.github.com>
2025-05-16 11:41:46 +0500

    fix:quotes in testimonials

--- app/test-page/sections/Testimonials.tsx
+++ app/test-page/sections/Testimonials.tsx
@@ -79,26 +79,15 @@ export default function Testimonials() {
             }}
           >
         
-              <div className="text-indigo-500">
-                <svg width="50" height="80" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M10 11H6C5.46957 11 4.96086 10.7893 4.58579 10.4142C4.21071 10.0391 4 9.53043 4 9V7C4 6.46957 4.21071 5.96086 4.58579 5.58579C4.96086 5.21071 5.46957 5 6 5H8C8.53043 5 9.03914 5.21071 9.41421 5.58579C9.78929 5.96086 10 6.46957 10 7V17C10 17.5304 9.78929 18.0391 9.41421 18.4142C9.03914 18.7893 8.53043 19 8 19H6C5.46957 19 4.96086 18.7893 4.58579 18.4142C4.21071 18.0391 4 17.5304 4 17V15"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="currentColor"
+                        <div className="text-indigo-500">
+                  <Image
+                    src="/test-page/testimonials/quotes.svg"
+                    alt="Quote Icon"
+                    width={50}
+                    height={80}
                   />
-                  <path
-                    d="M20 11H16C15.4696 11 14.9609 10.7893 14.5858 10.4142C14.2107 10.0391 14 9.53043 14 9V7C14 6.46957 14.2107 5.96086 14.5858 5.58579C14.9609 5.21071 15.4696 5 16 5H18C18.5304 5 19.0391 5.21071 19.4142 5.58579C19.7893 5.96086 20 6.46957 20 7V17C20 17.5304 19.7893 18.0391 19.4142 18.4142C19.0391 18.7893 18.5304 19 18 19H16C15.4696 19 14.9609 18.7893 14.5858 18.4142C14.2107 18.0391 14 17.5304 14 17V15"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="currentColor"
-                  />
-                </svg>
-              </div>
+                </div>
+
               <p 
                 className="text-gray-600  p-12"
                 style={{
