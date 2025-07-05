5ce7b7a97f607a8e4442b8b027d0583c74b24f99
raheebgill29 <140135207+raheebgill29@users.noreply.github.com>
2025-07-05 11:09:42 +0500

    fixes

--- app/platform/sections/extended-view/extended-view.tsx
+++ app/platform/sections/extended-view/extended-view.tsx
@@ -7,16 +7,12 @@ import SecuritySection from './security-section';
 
 export default function ExtendedView() {
   return (
-    <div className="relative left-1/2 -translate-x-1/2 w-[95vw] max-w-[1800px]">
-      <div>
-        <div className="p-8 rounded-lg mt-4 overflow-x-auto" style={{
-          background: ''
-        }}>
-          <div className="max-w-[1400px] mx-auto space-y-8">
-            <AgentsSection />
-            <EnterpriseSearchSection />
-            <SecuritySection />
-          </div>
+    <div className="w-full">
+      <div className="overflow-x-auto">
+        <div className="space-y-8">
+          <AgentsSection />
+          <EnterpriseSearchSection />
+          <SecuritySection />
         </div>
       </div>
     </div>
