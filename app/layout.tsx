7bb90cf8751911ff1693966efcf0934e0e219ab8
raheebgill29 <140135207+raheebgill29@users.noreply.github.com>
2025-07-18 18:49:39 +0500

    scrfipt

--- app/layout.tsx
+++ app/layout.tsx
@@ -118,6 +118,13 @@ export default function RootLayout({
         </Script>
         <script id="vtag-ai-js" async src="https://r2.leadsy.ai/tag.js" data-pid="9wW4yMR3XyNFUEOL" data-version="062024">
         </script>
+        
+        {/* REB2B Script */}
+        <Script id="reb2b-script" strategy="afterInteractive">
+          {`
+            !function () {var reb2b = window.reb2b = window.reb2b || [];if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"];reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments);args.unshift(method);reb2b.push(args);return reb2b;};};for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);}reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true;script.src = "https://ddwl4m2hdecbv.cloudfront.net/b/" + key + "/LNKLDHPD53OJ.js.gz";var first = document.getElementsByTagName("script")[0];first.parentNode.insertBefore(script, first);};reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("LNKLDHPD53OJ");
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
         <FontAwesome />
